e539866c92c31792a41e1cea34e615afe486ea33
Chris Mems <christopher.mems@.com>
2022-11-01 16:54:26 -0500

    solve challenge 4

--- python_functions.py
+++ python_functions.py
@@ -1,26 +1,27 @@
 # Challenge 1
 
+def sum_to(num):
+  sum = 0
+  for n in range(1, num + 1):
+    sum += n
+  return sum
 
-# def sum_to(num):
-#   sum = 0
-#   for n in range(1, num + 1):
-#     sum += n
-#   return sum
-
-# print(sum_to(6))
+print(sum_to(6))
 
 # Challenge 2
-# def largest(list1):
-#   max = list1[0]
-#   for x in list1:
-#     if x > max:
-#       max = x
-#   return max
+
+def largest(list1):
+  max = list1[0]
+  for x in list1:
+    if x > max:
+      max = x
+  return max
   
-# print(largest([1, 2, 3, 4, 0]))
-# print(largest([10, 4, 2, 231, 91, 54]))
+print(largest([1, 2, 3, 4, 0]))
+print(largest([10, 4, 2, 231, 91, 54]))
 
 # Challenge 3
+
 def occurrences(str1, str2):
   return str1.count(str2)
 
@@ -28,3 +29,11 @@ print(occurrences('fleep floop', 'fe'))
 
 
 # Challenge 4
+
+def product(*args):
+  num = 1
+  for arg in args:
+    num *= arg
+  return num
+
+print(product(2, 5, 5))
