d81172bccc8c293cb4493f1a8c31f628de1cc94b
Chris Mems <christopher.mems@.com>
2022-11-01 15:24:15 -0500

    solve challenge 1

---
+++ python_functions.py
@@ -0,0 +1,18 @@
+# Challenge 1
+
+
+# def sum_to(num):
+#   sum = 0
+#   for n in range(1, num + 1):
+#     sum += n
+#   return sum
+
+# print(sum_to(6))
+
+# Challenge 2
+
+
+# Challenge 3
+
+
+# Challenge 4
