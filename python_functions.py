3820f3596e3ccdaa2eab626fa1d4a749a3484e36
Chris Mems <christopher.mems@.com>
2022-11-01 16:36:16 -0500

    solve challenge 3

--- python_functions.py
+++ python_functions.py
@@ -10,17 +10,21 @@
 # print(sum_to(6))
 
 # Challenge 2
-def largest(list1):
-  max = list1[0]
-  for x in list1:
-    if x > max:
-      max = x
-  return max
+# def largest(list1):
+#   max = list1[0]
+#   for x in list1:
+#     if x > max:
+#       max = x
+#   return max
   
-print(largest([1, 2, 3, 4, 0]))
-print(largest([10, 4, 2, 231, 91, 54]))
+# print(largest([1, 2, 3, 4, 0]))
+# print(largest([10, 4, 2, 231, 91, 54]))
 
 # Challenge 3
+def occurrences(str1, str2):
+  return str1.count(str2)
+
+print(occurrences('fleep floop', 'fe'))
 
 
 # Challenge 4
